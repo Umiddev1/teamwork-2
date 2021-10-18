b849fb2de055d1c96713246d70607d63226c2859
ShaxruhAtanazarov <nazarovshaxruh98@gmail.com>
2021-10-18 15:54:53 +0500

    a

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 "use strict"
+let headerButton = document.querySelector('.header__btn');
+let headerButtonFirstLine = document.querySelector('.first-line');
+let headerButtonSecondLine = document.querySelector('.second-line');
+let headerButtonThirdLine = document.querySelector('.third-line');
+let headerNav = document.querySelector('.header__nav');
+
+
+headerButton.addEventListener('click', function(){
+    headerButtonFirstLine.classList.toggle('first-line--active');
+    headerButtonSecondLine.classList.toggle('second-line--active');
+    headerButtonThirdLine.classList.toggle('third-line--active');
+    headerNav.classList.toggle('nav--active');
+});
